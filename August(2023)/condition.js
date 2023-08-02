07b5563d533049c7d1a32579edca6628eb3b1778
jahid <jahidmorol2@gmail.com>
2023-08-02 14:10:13 +0600

    conditional problem solved

---
+++ August(2023)/condition.js
@@ -0,0 +1,8 @@
+//Make any short condition using Short conditonals
+
+const coder = "Jahid";
+
+//make with using &&
+coder === "Jahid" && console.log("I love Jahid");
+
+coder === "Jahid" || console.log("I hate U");
