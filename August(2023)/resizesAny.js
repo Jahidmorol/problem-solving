9a0c71590cb154edbbd0cdff33d9ed4d365d7ced
jahid <jahidmorol2@gmail.com>
2023-08-02 13:53:55 +0600

    resize array done

---
+++ August(2023)/resizesAny.js
@@ -0,0 +1,13 @@
+// Resizes any in an array
+
+const myarr = [
+    "Mohammad",
+    "Jubair",
+    "Developer",
+    "Coder",
+    "Programmer",
+    "Coder",
+  ];
+
+  myarr.length = 5;
+  console.log(myarr);
